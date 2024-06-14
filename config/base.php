8ad8d875f1b07381ee44d1497549b09c4993673f
Nan-Yu-326 <106731192+Nan-Yu-326@users.noreply.github.com>
2024-06-15 00:52:04 +0800

    Update base.php

--- config/base.php
+++ config/base.php
@@ -1,1 +1,114 @@
 
+<?php return array (
+  'site_name' => '墨白网盘',
+  'password' => 'Mobai991022',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/upload/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+      0 => '',
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+      11 => 'ipa',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => true,
+    'public' => true,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+      4 => 'jpeg',
+    ),
+  ),
+  'client_secret' => 'Jjp8Q~qhXkvnyz-zw2HACRp3mwM7CEecoZS8bc6g',
+  'client_id' => '6de7f971-9832-43f6-a253-cdcd9e0bfd5f',
+  'redirect_uri' => 'https://pan.mbjymh.top/',
+  'refresh_token' => '0.ASsArzPGAekQLkmPbSadryVnR3H5520ymPZDolPNzZ4L_V_CAJg.AgABAAEAAAAtyolDObpQQ5VtlI4uGjEPAgDs_wUA9P8lK2Vx1MCB6uYcZcaePsCJUq7rMhigwK13dp5rHtb0N-jAf5_GZGZFhk5y3zUp6jZ3Xy_gsCi4I_F04A8s-Jx_Zy3S-2-VpUKbHlsHs_k9yycWmWLTUr_2wuhgUhMtvMAloB2KaL2sgWv-QIxAqyDpvXX4vVUJx2rxD0BiCkOh5bDeuDD5nfopjs0o3wnAG0KXJu3p4X_xUbRK2f_ISR57fg6cQ52i_eh420msP0PbY29HkVsF8be0C1IaDkpVvmMBuw9K_d_AvRoguMfoamdf20J5q75XHAca8brgEDYiKBkS35whRTfxmcckj_hjoUf1-Zvup_ntFBuLDtdDWX19NC8vwuISOrWfvU49KBMLUOMoFngmlfXo_0aNKYLGgY_GGg2Xjlu4s27rEbL4emJwXf0CNtPHg0_OpcXrxESb_LOa7uHwDEo4lMrFOtCDjoPqlMKDXIqnmQp7UDFIH0XsTfwpSuiI4ZY8F5pco4ARgwNKL42p40mAqm4lmDB5lIrw1PWNwu9KXZjsXaMLGxuPXNCEZIi_vB6uqv08dUM1R4JpIaVNH2hEday2N09W4pRh7fSKLHkN8tKmciqEtE90fJm8UC-70Z5fvmhVlCMzqSahr1W5je-3--KwptL3GyjIt0ELiXq7Hz-5auEhjkVAsc6LC5ld1njAhmfwfnmz6QDDfqhFpNb6UaMdRnOah0A7SqBThN7iz_ZmRi1DX-x5-NpkRTA4HOUbDmUiISGOUuoGFy_Juavrsgi8ERJWOG7HNh8xBzRfTUb2F7iNrYVpsYM_qjWoVMMGlWkplM3dB55M05_HJSOEzKQf',
+  'onedrive_hide' => 'Android
+iOS
+PC
+TV
+其他',
+  'onedrive_hotlink' => '',
+);
